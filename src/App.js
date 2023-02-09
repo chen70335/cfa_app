4c42b9582877de8ce98a245016e063b5c6ad95ea
nchen35 <nchen35@uw.edu>
2023-02-08 23:34:21 -0800

    Final

--- src/App.js
+++ src/App.js
@@ -24,44 +24,51 @@ function App() {
     render() {
       return (
         <form className="filter">
-          <label>
+          <div>
             Choose Unit:
             <select value={filter} onChange={event => setFilter(event.target.value)}>
               <option>Standard</option>
               <option>Imperial</option>
               <option>Metric</option>
             </select>
-          </label>
+          </div>
         </form>
       );
     }
   }
 
   const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-        console.log('success')
-      })
-      setLocation(''); // Resets search bar as empty string after user clicks 'enter'
-    }
+    event.preventDefault();
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+      console.log('success')
+    })
+    setLocation(''); // Resets search bar as empty string after user clicks 'enter'
   }
 
   return (
-    <div className="app">
+    // <head>
+    //   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous"></link>
+    //   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
+    // </head>
+    <div className="page">
       <div className="search-bar">
         <div className="search">
+          <form onSubmit={searchLocation}>
           <input
-          value={location}
-          onChange={event => setLocation(event.target.value)} // Need to set value of location to this state
-          onKeyPress={searchLocation} // calls function as soon as anything is entered
-          placeholder='Enter Location'
-          type='text'/>
+            value={location}
+            onChange={event => setLocation(event.target.value)} // Need to set value of location to this state
+            placeholder='Enter Location'
+            type='text'/>
+          <UnitFilter />
+          <button type="submit" class="btn btn-labeled btn-success">
+            <span><img url="./assets/search_icon.jpg" alt="search icon"></img></span>
+          </button>
+          </form>
         </div>
-        <UnitFilter />
       </div>
-      <div className="container">
+      <div className="content">
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
@@ -75,18 +82,18 @@ function App() {
         </div>
         {/* this code will only run if data is defined. Shortened if statement. */}
         {data.name !== undefined &&
-          <div className='bottom'>
+          <div class="d-flex flex-row" className='bottom'>
             <div className="feels">
+              <p>Feels Like...</p>
               {data.main ? <p className="bold">{data.main.feels_like.toFixed()} F</p> : null}
-              <p>Feels Like</p>
             </div>
             <div className="humidity">
+              <p>Current Humidity</p>
               {data.main ? <p className="bold">{data.main.humidity} %</p> : null}
-              <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null}
               <p>Wind Speed</p>
+              {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null}
             </div>
           </div>
         }
